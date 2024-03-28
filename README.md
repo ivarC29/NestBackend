7aebc116d57d2e5227ca089c43d90fe78eb5aa2a
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-03-28 18:38:10 -0500

    Crete auth resource, createUser, encryption and partial login

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nest Backend
+
+For initialize database.
+```
+docker compose up -d
+```
+Copy ```.env.template``` file and rename to .env
+fill connection URI inside to ```.env``` file
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,78 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from "bcryptjs";
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectModel( User.name ) private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10 ),
+        ...userData
+      });
+
+      await newUser.save();
+      const {password:_, ...user } = newUser.toJSON();
+      return user;
+    } catch ( error ) {
+      if ( error.code === 11000 ) {
+        throw new BadRequestException(`${ createUserDto.email } is already in use.`);
+      }
+      throw new InternalServerErrorException();
+    }
+
+  }
+
+  async login( loginDto: LoginDto ) {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if ( !user ) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+    
+    if ( !bcryptjs.compareSync( password, user.password ) ) {
+      throw new UnauthorizedException('Not valid credentials - password');
+    }
+
+    const { password:_, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+    }
+
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
